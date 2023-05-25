69d817d973eeed0e11b645dd7b9cf0d71dd96f42
offshujr04 <juniorshubham0@gmail.com>
2023-05-25 22:25:10 +0530

    changed txt to json file for F3

--- F3/F3data.py
+++ F3/F3data.py
@@ -1,113 +1,110 @@
 import requests
 import threading
 from bs4 import BeautifulSoup
+import json
 
 
 def f3_news_():
-    with open("F3\F3 News\\News.txt", 'w', encoding="utf-8") as f3news:
-        news_url = "https://www.fiaformula3.com/Latest"
-        news_content = requests.get(news_url)
-        soup = BeautifulSoup(news_content.content, "html5lib")
+    try:
+        with open("Racing_Hub/F3/F3News/News.json", 'w', encoding="utf-8") as f3news:
+            news_url = "https://www.fiaformula3.com/Latest"
+            news_content = requests.get(news_url)
+            soup = BeautifulSoup(news_content.content, "html5lib")
+            out = {}
 
-        news_class = soup.find_all("p", class_="font-text-body")
-        news_list = []
-        for newzz in news_class:
-            news_list.append(newzz.text)
+            news_class = soup.find_all("p", class_="font-text-body")
+            for i, newzz in enumerate(news_class):
+                out[str(i+1)] = {"news": newzz.text}
 
-        img_news = soup.find_all("img", class_="f1-cc--photo")
-        img_list = []
-        for news_image in img_news:
-            source = news_image['data-src']
-            img_list.append(source)
+            img_news = soup.find_all("img", class_="f1-cc--photo")
+            for i, news_image in enumerate(img_news):
+                source = news_image['data-src']
+                out[str(i+1)]["image"] = source
 
-        all_news = list(zip(img_list, news_list,))
-        for i in all_news:
-            f3news.write(f"{i}\n ")
-    f3news.close()
+            f3news.write(json.dumps(out, ensure_ascii=False))
+    except FileNotFoundError:
+        print("File not found.")
+    except PermissionError:
+        print("Permission denied.")
+
+
+f3_news_()
 
 
 def f3_Drivernames_standings():
-    with open("F3\Standings\Team Standings.txt", 'w', encoding="utf-8") as f3drivers:
-        driverstand_url = "https://www.fiaformula3.com/Standings/Driver?seasonId=180"
-        driverstand_content = requests.get(driverstand_url)
-        soup = BeautifulSoup(driverstand_content.content, "html5lib")
+    try:
+        with open("Racing_Hub/F3/Standings/DriverStandings.json", 'w', encoding="utf-8") as f3drivers:
+            driverstand_url = "https://www.fiaformula3.com/Standings/Driver?seasonId=180"
+            driverstand_content = requests.get(driverstand_url)
+            soup = BeautifulSoup(driverstand_content.content, "html5lib")
+            out = {}
 
-        driverposition_class = soup.find_all("div", class_="pos")
-        driverposition_list = []
-        for positions_driver in driverposition_class:
-            driverposition_list.append(positions_driver.text)
+            driverstand_class = soup.find_all(
+                "span", class_="visible-desktop-up")
+            for i, driver_standing in enumerate(driverstand_class):
+                out[str(i+1)] = {"name": driver_standing.text}
 
-        driverstand_class = soup.find_all("span", class_="visible-desktop-up")
-        driverstand_list = []
-        for driver_standing in driverstand_class:
-            driverstand_list.append(driver_standing.text)
+            driverpoints_class = soup.find_all("div", class_="total-points")
+            for i, points_driver in enumerate(driverpoints_class):
+                out[str(i+1)]["points"] = points_driver.text
 
-        driverpoints_class = soup.find_all("div", class_="total-points")
-        driverpoints_list = []
-        for points_driver in driverpoints_class:
-            driverpoints_list.append(points_driver.text)
-        driver_stats = list(
-            zip(driverposition_list, driverstand_list, driverpoints_list))
+            f3drivers.write(json.dumps(out, ensure_ascii=False))
+    except FileNotFoundError:
+        print("File not found.")
+    except PermissionError:
+        print("Permission denied.")
 
-        for i in driver_stats:
-            f3drivers.write(f"{i}\n")
-    f3drivers.close()
+
+f3_Drivernames_standings()
 
 
 def f3_teamstandings_():
-    with open("F3\Standings\Team Standings.txt", 'w', encoding="utf-8") as f3team:
-        teamstand_url = "https://www.fiaformula3.com/Standings/Team?seasonId=180"
-        teamstand_content = requests.get(teamstand_url)
-        soup = BeautifulSoup(teamstand_content.content, "html5lib")
+    try:
+        with open("Racing_Hub/F3/Standings/TeamStandings.json", 'w', encoding="utf-8") as f3team:
+            teamstand_url = "https://www.fiaformula3.com/Standings/Team?seasonId=180"
+            teamstand_content = requests.get(teamstand_url)
+            soup = BeautifulSoup(teamstand_content.content, "html5lib")
+            out = {}
 
-        teamposition_class = soup.find_all("div", class_="pos")
-        teamposition_list = []
-        for position_team in teamposition_class:
-            teamposition_list.append(position_team.text)
+            teamname_class = soup.find_all(
+                "span", class_="visible-desktop-down")
+            teamname_list = [
+                namesof_team.text for namesof_team in teamname_class]
+            for i, teamname in enumerate(teamname_list):
+                out[str(i+1)] = {"teamname": teamname}
 
-        teamname_class = soup.find_all("span", class_="visible-desktop-down")
-        teamname_list = []
-        for namesof_team in teamname_class:
-            teamname_list.append(namesof_team.text)
+            teampoints_class = soup.find_all("div", class_="total-points")
+            for i, points_per_team in enumerate(teampoints_class):
+                out[str(i+1)]["points"] = points_per_team.text
 
-        teampoints_class = soup.find_all("div", class_="total-points")
-        teampoints_list = []
-        for points_per_team in teampoints_class:
-            teampoints_list.append(points_per_team.text)
+            f3team.write(json.dumps(out, ensure_ascii=False))
+    except FileNotFoundError:
+        print("File not found.")
+    except PermissionError:
+        print("Permission denied.")
 
-        teamstandings = list(
-            zip(teamposition_list, teamname_list, teampoints_list))
 
-        for i in teamstandings:
-            f3team.write(f"{i}\n ")
-
-    f3team.close()
+f3_teamstandings_()
 
 
 def _f3dates_():
-    with open("F3\Calendar\F3 Calendar.txt", 'w', encoding="utf-8") as f3calendar:
+    with open("Racing_Hub\F3\Calendar\F3 Calendar.txt", 'w', encoding="utf-8") as f3calendar:
         calendar_url = "https://www.fiaformula3.com/Calendar"
         calendar_content = requests.get(calendar_url)
         soup = BeautifulSoup(calendar_content.content, "html5lib")
+        out = dict()
 
         all_card_class = soup.find("div", class_="container calendar-layout")
         round_class = all_card_class.find_all("p", class_="h6")
         round_list = []
-        for noof_rounds in round_class:
-            round_list.append(noof_rounds.text)
-
-        calendar_class = all_card_class.find_all("p", class_="date")
-        calendar_list = []
-        for calendar in calendar_class:
-            calendar_list.append(calendar.text)
 
         # Finished races
         card_class = soup.find_all("div", class_="container wrapper card-post")
         location_list = []
-        for card in card_class:
+        for i, card in enumerate(card_class, 1):
             location_class = card .find(class_="ellipsis")
             for race_location in location_class:
-                location_list.append(race_location.text)
+                out[str(i)] = {"loc": race_location.text, "status": "finished"}
 
         # Upcoming class
         upcoming_card_class = soup.find_all(
@@ -116,9 +113,14 @@ def _f3dates_():
         for upcoming_card in upcoming_card_class:
             upcoming_location_class = upcoming_card.find_all(
                 class_="ellipsis")
-            for this_upcoming in upcoming_location_class:
+            for i, this_upcoming in enumerate(upcoming_location_class, i+1):
                 upcoming_location_class_list.append(this_upcoming.text)
-
+                out[str(i)] = {"loc": this_upcoming.text, "status": "upcoming"}
+        calendar_class = all_card_class.find_all("p", class_="date")
+        calendar_list = []
+        for i, calendar in enumerate(calendar_class, 1):
+            out[str(i)]["date"] = calendar.text
+        print(out)
         racefixtures = location_list+upcoming_location_class_list
 
         allcalendar = list(zip(round_list, calendar_list, racefixtures))
@@ -128,6 +130,8 @@ def _f3dates_():
 
     f3calendar.close()
 
+
+# _f3dates_()
 # Started Threading for efficient running
 
 
